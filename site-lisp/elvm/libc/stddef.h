574a9157465a18b4e3b18e656970c59817475f5c
Yousuke Ushiki <citrus.yubeshi@gmail.com>
2016-11-11 23:43:43 +0900

    add files

---
+++ site-lisp/elvm/libc/stddef.h
@@ -0,0 +1,14 @@
+#ifndef ELVM_LIBC_STDDEF_H_
+#define ELVM_LIBC_STDDEF_H_
+
+#include "_builtin.h"
+
+#define NULL 0
+
+typedef unsigned long size_t;
+typedef unsigned long ptrdiff_t;
+typedef long off_t;
+
+#define offsetof(type, field) ((size_t) &((type *)0)->field)
+
+#endif  // ELVM_LIBC_STDDEF_H_
